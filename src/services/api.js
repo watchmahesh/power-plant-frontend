84da89258c4dac5665e13ddfb48779345cfea207
mahesh karki <karki22mahesh@gmail.com>
2023-11-27 14:41:36 +0545

    remove console.log

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 const serverapibaseurl = process.env.SERVER_API_BASE_URL;
-console.log(serverapibaseurl)
 const api = axios.create({
   baseURL: serverapibaseurl || 'http://localhost:8003/api/v1', // Replace with your API server endpoint
 });
